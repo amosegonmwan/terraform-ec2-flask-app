3ec8ec2e9d23b791dc822d0e9da653816a3dd7a0
Amos Egonmwan, PhD <48167887+amosegonmwan@users.noreply.github.com>
2025-03-03 15:47:09 -0700

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,19 +1,66 @@
-# Key Pair
 resource "aws_key_pair" "example" {
-  key_name   = "terraform-demo-key"
-  public_key = file(var.public_key_path)
+  key_name   = "terraform-demo"
+  public_key = file("~/.ssh/id_rsa.pub")
 }
 
-# EC2 Instance
+resource "aws_security_group" "webSg" {
+  name   = "web"
+  vpc_id = aws_vpc.myvpc.id
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "server" {
   ami                    = "ami-0261755bbcb8c4a84"
   instance_type          = var.instance_type
   key_name               = aws_key_pair.example.key_name
   vpc_security_group_ids = [aws_security_group.webSg.id]
   subnet_id              = aws_subnet.sub1.id
-  user_data              = file("user_data.sh")
 
   tags = {
     Name = "webserver"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("~/.ssh/id_rsa")
+    host        = self.public_ip
+  }
+
+  provisioner "file" {
+    source      = "app.py"
+    destination = "/home/ubuntu/app.py"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update -y",
+      "sudo apt-get install -y python3-pip",
+      "cd /home/ubuntu",
+      "sudo pip3 install flask",
+      "nohup sudo python3 app.py > app.log 2>&1 &"
+    ]
   }
 }
